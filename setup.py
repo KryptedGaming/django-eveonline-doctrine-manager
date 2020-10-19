f92fd91d77842635cf48c2192344ab6233c8e1e5
porowns <porowns@gmail.com>
2020-10-18 20:45:26 -0400

    Fix version truth, add krypted bindings

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-eveonline-doctrine-manager',
-    version='1.2.3',
+    version=__import__('django_eveonline_doctrine_manager').__version__,
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
